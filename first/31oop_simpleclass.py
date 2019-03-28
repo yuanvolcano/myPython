e8bce310e1e184de103e6f0f2b907d1769af1257
袁灿 <can_yuan@kingdee.com>
2019-03-28 21:55:18 +0800

    class

---
+++ first/31oop_simpleclass.py
@@ -0,0 +1,5 @@
+class Person:
+  pass # 空语句
+
+p = Person()
+print(p)
---
+++ first/32oop_method.py
@@ -0,0 +1,7 @@
+class Person:
+  def say_hi(self):
+    print('hello, how are you?')
+    print(self)
+
+p = Person()
+p.say_hi()
